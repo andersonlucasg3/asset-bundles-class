89ec60e98b307c9c0986fc837b71a08f0fe27791
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-06-02 12:01:15 -0300

    Corrigido path do build;

--- Assets/Sources/Editor/BuildSystem/GameBuilder.cs
+++ Assets/Sources/Editor/BuildSystem/GameBuilder.cs
@@ -9,17 +9,19 @@ namespace AssetBundlesClass.Editor.BuildSystem
     public static class GameBuilder
     {
         private static readonly string _activeBuildTargetName = $"{EditorUserBuildSettings.activeBuildTarget}";
-        private static readonly string _buildOutputPath = Path.Combine(Directory.GetParent(Application.dataPath)?.ToString() ?? "", _activeBuildTargetName);
+        private static readonly string _buildOutputPath = Path.Combine(Directory.GetParent(Application.dataPath)?.ToString() ?? "", "builds");
+        private static readonly string _buildPlatformPath = Path.Combine(_buildOutputPath, _activeBuildTargetName);
         
         [MenuItem("AssetBundlesClass/Game Builder/Build (Current Platform)")]
         public static void BuildCurrentPlatform()
         {
+            if (!Directory.Exists(_buildPlatformPath)) Directory.CreateDirectory(_buildPlatformPath);
             BuildPlayerOptions playerOptions = new BuildPlayerOptions
             {
                 options = BuildOptions.Development,
                 target = EditorUserBuildSettings.activeBuildTarget,
                 targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
-                locationPathName = _buildOutputPath,
+                locationPathName = _buildPlatformPath,
                 assetBundleManifestPath = Path.Combine(AssetBundlesBuilder.assetBundlesOutputPath, _activeBuildTargetName),
             };
             BuildReport report = BuildPipeline.BuildPlayer(playerOptions);
